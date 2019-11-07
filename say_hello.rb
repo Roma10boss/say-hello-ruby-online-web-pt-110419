813a6a576eb48e93067aedc1d592917f0bfdf708
Romario Gustave <romagustave10@gmail.com>
2019-11-07 22:31:52 +0000

    Automatically backed up by Learn

--- say_hello.rb
+++ say_hello.rb
@@ -1,6 +1,6 @@
 # Build your say_hello method here
-def say_hello(language="Ruby")
-  puts "Hello #{language} programmer!"
+def say_hello()
+  puts "Hello Ruby programmer!"
   end
 def say_hello(name = "Gabriela")
   puts "Hello #{name}!"
